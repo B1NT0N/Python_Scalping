46b0cf7f680fda5030980d3a1f13568517f44cac
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-07-27 23:20:12 -0100

    NameYearIdLinkAllDone

--- Tests.py
+++ Tests.py
@@ -1,17 +1,4 @@
-from urllib.request import urlopen as uRequest
-from bs4 import BeautifulSoup as soup
-
-uClient = uRequest("https://www.imdb.com/title/tt1560954/?ref_=fn_ft_tt_1")
-page_html_link = uClient.read()
-uClient.close()
-
-page_soup_link = soup(page_html_link, "html.parser")
-
-
-containers_link1 = page_soup_link.findAll("div",{ "class":"TitleBlock__Container-sc-1nlhx7j-0 hglRHk"})
-
-for container in containers_link1:  
-    containers_link2=containers_link1[container].findAll("div",{ "class":"TitleBlock__TitleContainer-sc-1nlhx7j-1 jxsVNt"})
-    containers_link3=containers_link2.findAll("div",{ "class":"TitleBlock__TitleMetaDataContainer-sc-1nlhx7j-2 hWHMKr"})
-
-print(containers_link1)
+name=["OK","0K","KO","OK"]
+for x in range(len(name)):
+    if name[x].find("OK") != -1:
+        print("yes")
--- WS.py
+++ WS.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen as uRequest
 from bs4 import BeautifulSoup as soup
 
+
 class Entertainment:
 
     def __init__(self,name,year,id,link):
@@ -12,7 +13,7 @@ class Entertainment:
 #srh=input("Search:")
 #srh=srh.replace(" ","+")
 srh = "gun"
-my_url = "https://www.imdb.com/find?q=assasin&s=tt&ttype=ft&ref_=fn_ft"
+my_url = "https://www.imdb.com/find?s=tt&q=13&ref_=nv_sr_sm"
 
 uClient = uRequest(my_url)
 page_html = uClient.read()
@@ -33,30 +34,50 @@ for x in range(len(containers)):
 name=[]
 links=[]
 other=[]
+ID=[]
+id=[]
 info=results.table.findAll("tr")
 for x in range(len(info)):
-    name.append(info[x].find("td",{"class":"result_text"}).a.text)
+    name.append(info[x].find("td",{"class":"result_text"}).text)
     other.append(info[x].find("td",{"class":"result_text"}).text.split())
-    links.append("https://www.imdb.com"+info[1].find("td",{"class":"result_text"}).a["href"])
+    ID.append(info[x].find("td",{"class":"result_text"}).a["href"].split("/"))
+    links.append("https://www.imdb.com"+info[x].find("td",{"class":"result_text"}).a["href"])
 
-#for link in links:
-    uClient = uRequest("https://www.imdb.com/title/tt1560954/?ref_=fn_ft_tt_1")
-    page_html_link = uClient.read()
-    uClient.close()
+for x in range(len(ID)):
+    id.append(ID[x][2])
 
-    page_soup_link = soup(page_html_link, "html.parser")
 
-    containers_link = page_soup.findAll("div",{ "class":"TitleBlock__Container"})
+date_filter=[]
+year = []
+for x in range(len(other)):
+    for y in range(len(other[x])):
+        if len(other[x][y]) == 6:
+            date_filter.append(other[x][y].replace('(', '').replace(')', ''))
+        elif other[x][y] == "development)":
+            date_filter.append(other[x][y].replace('(', '').replace(')', ''))
+
+for date in range(len(date_filter)):
+    if date_filter[date].isnumeric() == True:
+            year.append(date_filter[date])
+    elif date_filter[date] == "development":
+        year.append(date_filter[date])
 
 f_result=[]
-#for x in range(len(other)):
-#    f_result.append(Entertainment(name[x],year[x],"-----",links[x]))
-#    print(f"Name:{f_result[x].name} | Year:{f_result[x].year} | ID:{f_result[x].id}")
+for x in range(len(name)):
+    for y in range(len(year)):
+        if name[x].find(year[y]) != -1:
+            f_result.append(Entertainment(name[x],year[y],id[x],links[x]))
+            break
+        elif y == (len(year)-1):
+            f_result.append(Entertainment(name[x],"-----",id[x],links[x]))
+        else:
+            pass
+    print(f"Name:{f_result[x].name} | Year: {f_result[x].year} | ID: {f_result[x].id} | Link:{f_result[x].link}")
 
-#print(yearr)
+#print(id)
 #print(yearn1)
-#print(yearn2)
-print(containers_link)
+#print(year)
+#print(name)
 
 #for i in range(len(data)):
 #    print(f"Name:{data[i][0]} | Year:{data[i][1]}")
